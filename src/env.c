093bd1d39c4198da7a84754835ddfa95388eacbf
Dylan Romansky <dromansk@e1z2r8p6.42.us.org>
2019-07-04 20:04:49 -0700

    added get_next_line for getting actual input

--- src/env.c
+++ src/env.c
@@ -6,13 +6,16 @@
 /*   By: dromansk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 17:29:25 by dromansk          #+#    #+#             */
-/*   Updated: 2019/06/26 20:54:03 by dromansk         ###   ########.fr       */
+/*   Updated: 2019/07/03 23:10:05 by dromansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minishell.h"
 
 /* might need to clone environ so I can work with it within this terminal */
+/* did clone environ to work with within this terminal */
+/* path exists as part of environ so that's easy enough */
+/* might not need to clone environment */
 
 char	**ft_env_set(char *new, char **env)
 {
@@ -29,4 +32,17 @@ char	**ft_env_set(char *new, char **env)
 	else
 		array_join(env, new);
 	return (env);
+}
+
+int		execute_com(char *com, char **env)
+{
+	int		i;
+	char	**path;
+
+	i = 0;
+	while (!ft_strnstr(env[i], "PATH=", 5))
+		i++;
+	path = ft_strsplit(env[i] + 5, ':');
+	//either concatonate the command onto each string of path and find file that way
+	//or find a way to search that path for an executable 
 }
